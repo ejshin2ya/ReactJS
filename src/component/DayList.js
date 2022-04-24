9e29e4a5798596edc4151a4da0d262ebfb754a66
ejshin2ya <lovekiej92@gmail.com>
2022-04-24 12:53:52 +0900

    React JS #12 useEffect, fetch()로 API 호출

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import dummy from "../db/data.json";
 
 export default function DayList() {
+  const [days, setDays] = useState([]); //dummy 사용하던 것을 api에서 리스트를 가져와서 바꾸는 과정(days라는 useState 사용)
+
+  //api 비동기 통신을 위해 fetch 사용
+  useEffect(() => {
+    fetch("http://localhost:3001/days")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setDays(data);
+      });
+  }, []);
+
   return (
     <ul className="list_day">
-      {dummy.days.map((day) => (
+      {days.map((day) => (
         <li key={day.id}>
           <Link to={`/day/${day.day}`}>Day {day.day}</Link>
         </li>
